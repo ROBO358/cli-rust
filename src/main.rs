2a5ae685fef609eba9cdbedd2da7864a0e28e5a7
TSURUDO Ryosuke <18186312+ROBO358@users.noreply.github.com>
2024-09-13 23:22:39 +0900

    New: subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, flatten_help = true)]
+struct Args {
+    #[command(subcommand)]
+    sub: Command,
+}
+
+#[derive(Subcommand, Clone, Debug)]
+enum Command {
+    Hoge {
+        fuga: String,
+    }
+}
+
+fn print(fuga: &str) {
+    println!("fuga: {:?}", fuga)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    match Args::parse().sub {
+        Command::Hoge { fuga } => print(&fuga),
+    }
 }
